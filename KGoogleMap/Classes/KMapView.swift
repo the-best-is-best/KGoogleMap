53e16763c1b044343da4345c61b65a0bc0b4e6f3
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-09-29 20:33:33 +0300

    fix KMapView not compiled

--- KGoogleMap/Classes/KMapView.swift
+++ KGoogleMap/Classes/KMapView.swift
@@ -2,84 +2,83 @@ import SwiftUI
 import GoogleMaps
 import CoreLocation
 
+@objcMembers public final class KMapViewCoordinator: NSObject, CLLocationManagerDelegate {
+    var mapView: GMSMapView?
+    var locationManager = CLLocationManager()
+    var previousLocation: CLLocation?
+    var polyline: GMSPolyline?
+    @Binding var totalDistance: CLLocationDistance
+
+    init(totalDistance: Binding<CLLocationDistance>) {
+        self._totalDistance = totalDistance
+        super.init()
+        
+        // Set up the location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    // Location manager delegate method
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+
+        // Calculate distance and update total distance
+        if let previousLocation = self.previousLocation {
+            let distance = location.distance(from: previousLocation)
+            self.totalDistance += distance
+        }
+
+        // Update polyline to draw the route
+        if let path = polyline?.path {
+            let mutablePath = GMSMutablePath(path: path)
+            mutablePath.add(location.coordinate)
+            polyline?.path = mutablePath
+        } else {
+            let path = GMSMutablePath()
+            path.add(location.coordinate)
+            
+            let newPolyline = GMSPolyline(path: path)
+            newPolyline.strokeColor = .red
+            newPolyline.strokeWidth = 5.0
+            newPolyline.map = mapView
+            polyline = newPolyline
+        }
+
+        // Set camera position to current location
+        let zoom: Float = 18.0
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoom)
+        mapView?.animate(to: camera)
+
+        self.previousLocation = location
+    }
+
+    // Authorization status change handler
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse || status == .authorizedAlways {
+            locationManager.startUpdatingLocation()
+            mapView?.isMyLocationEnabled = true
+            mapView?.settings.myLocationButton = true
+        } else {
+            // Handle authorization denied state if needed
+            locationManager.stopUpdatingLocation()
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Manager failed with error: \(error.localizedDescription)")
+    }
+}
+
 public struct KMapView: UIViewRepresentable {
     // Binding for total distance
     @Binding var totalDistance: CLLocationDistance
 
-    // Coordinator to handle events
-    public class Coordinator: NSObject, CLLocationManagerDelegate {
-        var mapView: GMSMapView?
-        var locationManager = CLLocationManager()
-        var previousLocation: CLLocation?
-        var polyline: GMSPolyline?
-        @Binding var totalDistance: CLLocationDistance
-
-        init(totalDistance: Binding<CLLocationDistance>) {
-            self._totalDistance = totalDistance
-            super.init()
-            
-            // Set up the location manager
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestWhenInUseAuthorization()
-        }
-
-        // Location manager delegate method
-        public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            guard let location = locations.first else { return }
-
-            // Calculate distance and update total distance
-            if let previousLocation = self.previousLocation {
-                let distance = location.distance(from: previousLocation)
-                self.totalDistance += distance
-            }
-
-            // Update polyline to draw the route
-            if let path = polyline?.path {
-                let mutablePath = GMSMutablePath(path: path)
-                mutablePath.add(location.coordinate)
-                polyline?.path = mutablePath
-            } else {
-                let path = GMSMutablePath()
-                path.add(location.coordinate)
-                
-                let newPolyline = GMSPolyline(path: path)
-                newPolyline.strokeColor = .red
-                newPolyline.strokeWidth = 5.0
-                newPolyline.map = mapView
-                polyline = newPolyline
-            }
-
-            // Set camera position to current location
-            let zoom: Float = 18.0
-            let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
-                                                  longitude: location.coordinate.longitude,
-                                                  zoom: zoom)
-            mapView?.animate(to: camera)
-
-            self.previousLocation = location
-        }
-
-        // Authorization status change handler
-        public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-            if status == .authorizedWhenInUse || status == .authorizedAlways {
-                locationManager.startUpdatingLocation()
-                mapView?.isMyLocationEnabled = true
-                mapView?.settings.myLocationButton = true
-            } else {
-                // Handle authorization denied state if needed
-                locationManager.stopUpdatingLocation()
-            }
-        }
-
-        public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-            print("Location Manager failed with error: \(error.localizedDescription)")
-        }
-    }
-
     // Make coordinator
-    public func makeCoordinator() -> Coordinator {
-        return Coordinator(totalDistance: $totalDistance)
+    public func makeCoordinator() -> KMapViewCoordinator {
+        return KMapViewCoordinator(totalDistance: $totalDistance)
     }
 
     // Make UIView
@@ -91,8 +90,7 @@ public struct KMapView: UIViewRepresentable {
         // Set coordinator's mapView
         context.coordinator.mapView = mapView
 
-        // Request location authorization and start updating location
-        context.coordinator.locationManager.requestWhenInUseAuthorization()
+        // Start updating location
         context.coordinator.locationManager.startUpdatingLocation()
         
         return mapView
