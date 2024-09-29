7cba6a6d2d815e0eed3418422bc3616caf017723
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-09-29 22:48:07 +0300

    fix

--- KGoogleMap/Classes/KMapView.swift
+++ KGoogleMap/Classes/KMapView.swift
@@ -16,7 +16,10 @@ import GoogleMaps
     // Public property for total distance, exposing it for Objective-C
     @objc public var totalDistance: CLLocationDistance {
         get { _totalDistance }
-        set { _totalDistance = newValue }
+        set {
+            _totalDistance = newValue
+            // If needed, you can update the SwiftUI view here
+        }
     }
 
     // Initializer accepting CLLocationDistance
@@ -25,19 +28,20 @@ import GoogleMaps
         super.init(frame: .zero)
         setupView()
     }
-    
+
+    // Required initializer for UIView
+    override public init(frame: CGRect) {
+        self._totalDistance = 0 // or any default value you prefer
+        super.init(frame: frame)
+        setupView()
+    }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     private func setupView() {
-        // Create a Binding to use in the representable
-        let totalDistanceBinding = Binding<CLLocationDistance>(
-            get: { self._totalDistance },
-            set: { self._totalDistance = $0 }
-        )
-
-        let representable = KMapViewRepresentable(totalDistance: totalDistanceBinding)
+        let representable = KMapViewRepresentable(totalDistance: .constant(_totalDistance))
         let swiftUIController = UIHostingController(rootView: representable)
         swiftUIController.view.frame = bounds
         swiftUIController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
